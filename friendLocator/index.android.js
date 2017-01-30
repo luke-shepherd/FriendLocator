af46655736dcc4ba0c8698a3f721cffc8083e9ea
k-danna <kdanna@ucsc.edu>
2017-01-30 11:00:06 -0800

    added navigator with page stack, login button

--- friendLocator/index.android.js
+++ friendLocator/index.android.js
@@ -9,10 +9,13 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  View
+  Button,
+  View,
+  Navigator
 } from 'react-native';
 
-export default class AllThingsLogin extends Component {
+export default class MainApp extends Component {
+    /* creates state object on initialization */
     constructor(props) {
         super(props);
         this.state = {
@@ -21,6 +24,7 @@ export default class AllThingsLogin extends Component {
         };
     }
 
+    /* called on each character press, logs username in state*/
     updateUsername = (text) => {
         this.setState((state) => {
             return {
@@ -30,6 +34,7 @@ export default class AllThingsLogin extends Component {
         });
     };
 
+    /* called on each character press, logs password in state*/
     updatePassword = (text) => {
         this.setState((state) => {
             return {
@@ -39,20 +44,22 @@ export default class AllThingsLogin extends Component {
         });
     };
 
+    /*only allow certain characters, word ban list, etc, etc*/
     checkText = (text) => {
-        /*only allow certain characters, word ban list, etc*/
-        console.log('checking: ' + text)
+        console.log('[*] checkText(): ' + text)
     };
 
-    loginUser = (text) => {
-        /*send username, password to server*/
-        console.log('logging in: ' + text)
+    /*send username, password to server*/
+    loginUser = (text, route, routes) => {
+        console.log('[*] loginUser(): ' + this.state.username 
+                + ', ' + this.state.password)
 
-        console.log('type: ')
-        console.log(typeof this.state.username)
+        /* DEBUG: always successful */
+        console.log('[+] login: successful')
+        return true
 
+        /* 
         var url = 'http://107.170.249.224:3000/test';
-        /*var url = 'http://www.reddit.com/';*/
 
         var obj = {
             method: 'POST',
@@ -61,70 +68,128 @@ export default class AllThingsLogin extends Component {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                'user': 'adult',
-                'pass': 'friends',
+                'user': this.state.username,
+                'pass': this.state.password,
             })
         };
 
         let response = fetch(url, obj)
         .then((response) => response)
         .then((responseJson) => {
-            console.log('response: ')
+            console.log('[*] response: ')
             console.log(responseJson);
+            if (false) {
+                console.log('[+] login: success')
+                return true
+            }
+            else {
+                console.log('[-] login: failure')
+                return false
+            }
         })
         .catch((error) => {
-            console.log('error: ')
+            console.log('[*] error: ')
             console.log(error);
         });
+        */
 
     };
 
+    /* render a page stack */
     render() {
+        const routes = [
+            {title: 'loginpage', index: 0},
+            {title: 'mappage', index: 1},
+        ];
         return (
-            <View style={styles.logincontainer}>
-                <Text style={styles.logintxt}>
-                    Adults with Friends
-                </Text>
-                <TextInput 
-                    style={styles.loginbox}
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    placeholder='Enter Username!'
-                    onChange={(event) => this.updateUsername(
-                        event.nativeEvent.text
-                    )}
+            /*  page stack, controls what page is rendered */
+            <Navigator
+                initialRoute={routes[0]}
+                initialRouteStack={routes}
+                renderScene={(route, navigator) =>
 
-                    onEndEditing={(event) => this.checkText(
-                        event.nativeEvent.text
-                    )}
-                />
-                <TextInput 
-                    style={styles.loginbox}
-                    autoCapitalize='none'
-                    autoCorrect={false}
-                    placeholder='Enter Password!'
-                    onChange={(event) => this.updatePassword(
-                        event.nativeEvent.text
-                    )}
+                /* a container to use with flex */
+                <View style={styles.logincontainer}>
+                    
+                    {/* simple text title */}
+                    <Text style={styles.logintxt}>
+                        Adults with Friends
+                    </Text>
+                    
+                    {/* field so user can enter username */}
+                    <TextInput 
+                        style={styles.loginbox}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        placeholder='Enter Username!'
+                        /* event called on character press */
+                        onChange={(event) => this.updateUsername(
+                            event.nativeEvent.text
+                        )}
+                        /* event called when exiting the box */
+                        onEndEditing={(event) => this.checkText(
+                            event.nativeEvent.text
+                        )}
+                    />
+                    
+                    {/* field so user can enter password */}
+                    <TextInput 
+                        style={styles.loginbox}
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        placeholder='Enter Password!'
+                        /* event called on character press */
+                        onChange={(event) => this.updatePassword(
+                            event.nativeEvent.text
+                        )}
+                        /* event called when exiting the box */
+                        /* FIXME: onEnd not called here due to user never
+                                  leaving the box, just goes directly 
+                                  to onSubmit  */
+                        onEndEditing={(event) => this.checkText(
+                            event.nativeEvent.text
+                        )}
+                        /* event called when user hits enter */
+                        onSubmitEditing={(event) => {
+                            /* push next page onto stack on login success */
+                            if (this.loginUser(event.nativeEvent.text)) {
+                                console.log('[+] rendering next page')
+                                if (route.index == 0) {
+                                    navigator.push(routes[1])
+                                }
+                            }
 
-                    onEndEditing={(event) => this.checkText(
-                        event.nativeEvent.text
-                    )}
+                        }}
+                    />
 
-                    onSubmitEditing={(event) => this.loginUser(
-                        event.nativeEvent.text
-                    )}
-                />
-                <Text style={styles.logintxt}>
-                    user: {this.state.username}{'\n'}
-                    pass: {this.state.password}
-                </Text>
-            </View>
+                    {/* quick button for submit */}
+                    <Button
+                        onPress={(event) => {
+                            /* push next page onto stack on login success */
+                            if (this.loginUser(event.nativeEvent.text)) {
+                                console.log('[+] rendering next page')
+                                if (route.index == 0) {
+                                    navigator.push(routes[1])
+                                }
+                            }
+                        }}
+                        title='login'
+                        color='skyblue'
+                    />
+
+                    {/* displays current state as text for debug */}
+                    <Text style={styles.logintxt}>
+                        user: {this.state.username}{'\n'}
+                        pass: {this.state.password}
+                    </Text>
+                </View>
+            }/>
         );
     }
 
 }
 
+/* css stylesheet object */
 const styles = StyleSheet.create({
   logincontainer: {
     flex: 1, //allows flex usage for justification
@@ -142,5 +207,6 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('friendLocator', () => AllThingsLogin);
+/* register the main class; the app name and key below must be the same */
+AppRegistry.registerComponent('friendLocator', () => MainApp);
 

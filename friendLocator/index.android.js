b199595c56cd03600a57ffc0572624ab870f502e
k-danna <kdanna@ucsc.edu>
2017-01-30 16:03:56 -0800

    started restructuring code, renamed prev code to .old and started with fresh file

--- friendLocator/index.android.js
+++ friendLocator/index.android.js
@@ -1,6 +1,7 @@
 /**
- * Login Page for cs183 project
- * https://github.com/luke-shepherd/AdultFriendLocator
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
  */
 
 import React, { Component } from 'react';
@@ -8,205 +9,40 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  TextInput,
-  Button,
-  View,
-  Navigator
+  View
 } from 'react-native';
 
-export default class MainApp extends Component {
-    /* creates state object on initialization */
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-        };
-    }
+/*
+    goals: 
+        solve navigation problem
+        create a page class in separate file
+        render it here
+*/
 
-    /* called on each character press, logs username in state*/
-    updateUsername = (text) => {
-        this.setState((state) => {
-            return {
-                username: text,
-                password: this.state.password,
-            }
-        });
-    };
-
-    /* called on each character press, logs password in state*/
-    updatePassword = (text) => {
-        this.setState((state) => {
-            return {
-                username: this.state.username,
-                password: text,
-            }
-        });
-    };
-
-    /*only allow certain characters, word ban list, etc, etc*/
-    checkText = (text) => {
-        console.log('[*] checkText(): ' + text)
-    };
-
-    /*send username, password to server*/
-    loginUser = (text, route, routes) => {
-        console.log('[*] loginUser(): ' + this.state.username 
-                + ', ' + this.state.password)
-
-        /* DEBUG: always successful */
-        console.log('[+] login: successful')
-        return true
-
-        /* 
-        var url = 'http://107.170.249.224:3000/test';
-
-        var obj = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                'user': this.state.username,
-                'pass': this.state.password,
-            })
-        };
-
-        let response = fetch(url, obj)
-        .then((response) => response)
-        .then((responseJson) => {
-            console.log('[*] response: ')
-            console.log(responseJson);
-            if (false) {
-                console.log('[+] login: success')
-                return true
-            }
-            else {
-                console.log('[-] login: failure')
-                return false
-            }
-        })
-        .catch((error) => {
-            console.log('[*] error: ')
-            console.log(error);
-        });
-        */
-
-    };
-
-    /* render a page stack */
-    render() {
-        const routes = [
-            {title: 'loginpage', index: 0},
-            {title: 'mappage', index: 1},
-        ];
-        return (
-            /*  page stack, controls what page is rendered */
-            <Navigator
-                initialRoute={routes[0]}
-                initialRouteStack={routes}
-                renderScene={(route, navigator) =>
-
-                /* a container to use with flex */
-                <View style={styles.logincontainer}>
-                    
-                    {/* simple text title */}
-                    <Text style={styles.logintxt}>
-                        Adults with Friends
-                    </Text>
-                    
-                    {/* field so user can enter username */}
-                    <TextInput 
-                        style={styles.loginbox}
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                        placeholder='Enter Username!'
-                        /* event called on character press */
-                        onChange={(event) => this.updateUsername(
-                            event.nativeEvent.text
-                        )}
-                        /* event called when exiting the box */
-                        onEndEditing={(event) => this.checkText(
-                            event.nativeEvent.text
-                        )}
-                    />
-                    
-                    {/* field so user can enter password */}
-                    <TextInput 
-                        style={styles.loginbox}
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                        placeholder='Enter Password!'
-                        /* event called on character press */
-                        onChange={(event) => this.updatePassword(
-                            event.nativeEvent.text
-                        )}
-                        /* event called when exiting the box */
-                        /* FIXME: onEnd not called here due to user never
-                                  leaving the box, just goes directly 
-                                  to onSubmit  */
-                        onEndEditing={(event) => this.checkText(
-                            event.nativeEvent.text
-                        )}
-                        /* event called when user hits enter */
-                        onSubmitEditing={(event) => {
-                            /* push next page onto stack on login success */
-                            if (this.loginUser(event.nativeEvent.text)) {
-                                console.log('[+] rendering next page')
-                                if (route.index == 0) {
-                                    navigator.push(routes[1])
-                                }
-                            }
-
-                        }}
-                    />
-
-                    {/* quick button for submit */}
-                    <Button
-                        onPress={(event) => {
-                            /* push next page onto stack on login success */
-                            if (this.loginUser(event.nativeEvent.text)) {
-                                console.log('[+] rendering next page')
-                                if (route.index == 0) {
-                                    navigator.push(routes[1])
-                                }
-                            }
-                        }}
-                        title='login'
-                        color='skyblue'
-                    />
-
-                    {/* displays current state as text for debug */}
-                    <Text style={styles.logintxt}>
-                        user: {this.state.username}{'\n'}
-                        pass: {this.state.password}
-                    </Text>
-                </View>
-            }/>
-        );
-    }
-
+export default class Main extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.txt}>
+          Something you did worked.
+        </Text>
+      </View>
+    );
+  }
 }
 
-/* css stylesheet object */
 const styles = StyleSheet.create({
-  logincontainer: {
-    flex: 1, //allows flex usage for justification
+  container: {
+    flex: 1,
     justifyContent: 'center',
-    //alignItems: 'center',
-    backgroundColor: 'steelblue',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  loginbox: {
-    height: 100,
-  },
-  logintxt: {
-    fontSize: 50,
+  txt: {
+    fontSize: 20,
     textAlign: 'center',
-    color: 'skyblue',
+    margin: 10,
   },
 });
 
-/* register the main class; the app name and key below must be the same */
-AppRegistry.registerComponent('friendLocator', () => MainApp);
-
+AppRegistry.registerComponent('friendLocator', () => Main);
